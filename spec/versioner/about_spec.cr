946eefc5993981fc1aa60b27b9299d5737807f2f
Daniel Huffman <drhuffman12@gmail.com>
2025-07-26 23:27:44 -0400

    2025-07-26b wip add parsing version from readme

--- spec/versioner/about_spec.cr
+++ spec/versioner/about_spec.cr
@@ -5,5 +5,9 @@ describe Versioner do
   it "version in shard.yml matches version in Versioner::About::VERSION" do
     (`shards version .`).strip.should eq(Versioner::About::VERSION)
   end
+
+  it "fails" do
+    "forced failure".should eq("a forced failure")
+  end
 end
 
